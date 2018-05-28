ad5bd8ae7860e25d58100d6f55528f1184a7b60e
GiorgiosaudOld <jorgelsaud@gmail.com>
2018-05-28 16:01:05 -0400

    Revert "year picker working"
    This reverts commit 7f3c939bbc9159af094f6d5e2e7d121e95dc3ecc.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import './polyfills'
 import Datepicker from './components/AirbnbStyleDatepicker.vue'
 import Monthpicker from './components/AirbnbStyleMonthpicker.vue'
-import Yearpicker from './components/AirbnbStyleYearpicker.vue'
 import ClickOutside from './directives/ClickOutside'
 
 const AirbnbStyleDatepicker = {
@@ -25,21 +24,10 @@ const AirbnbStyleMonthpicker = {
     })
   }
 }
-const AirbnbStyleYearpicker = {
-  install(Vue, options) {
-    Vue.directive('click-outside', ClickOutside)
-
-    Vue.component(Yearpicker.name, {
-      ...options,
-      ...Yearpicker
-    })
-  }
-}
 // User has to install the component by themselves, to allow to pass options
 if (typeof window !== 'undefined' && window.Vue) {
   window.AirbnbStyleDatepicker = AirbnbStyleDatepicker
   window.AirbnbStyleMonthpicker = AirbnbStyleMonthpicker
-  window.AirbnbStyleYearpicker = AirbnbStyleYearpicker
 }
 
-export { AirbnbStyleDatepicker, AirbnbStyleMonthpicker, AirbnbStyleYearpicker }
+export { AirbnbStyleDatepicker, AirbnbStyleMonthpicker }
